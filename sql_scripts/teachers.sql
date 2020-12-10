9c84b157678360f0cb7c068ba9d43bc245e3fd94
tiewkeehui <keehuitiew@airasia.com>
2020-12-10 13:45:51 +0800

    Fix teacher.sql

--- sql_scripts/teachers.sql
+++ sql_scripts/teachers.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `teachers` (
   `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-  `teacher_id` int(9) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `subject` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
-INSERT INTO `teachers` (`id`, `teacher_id`, `subject`) VALUES (1, 1, 'Maths');
-INSERT INTO `teachers` (`id`, `teacher_id`, `subject`) VALUES (2, 2, 'Geography');
-INSERT INTO `teachers` (`id`, `teacher_id`, `subject`) VALUES (3, 3, 'English');
+INSERT INTO `teachers` (`id`, `title`, `name`, `subject`) VALUES (1, 'Dr', 'Smith Brown', 'Maths');
+INSERT INTO `teachers` (`id`, `title`, `name`, `subject`) VALUES (2, 'Mr', 'David James', 'Geography');
+INSERT INTO `teachers` (`id`, `title`, `name`, `subject`) VALUES (3, 'Miss', 'Rebecca White', 'English');
