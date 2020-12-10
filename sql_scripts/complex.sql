f905be710161babc6e799afbf378adcfa7e22434
tiewkeehui <keehuitiew@airasia.com>
2020-12-10 14:39:12 +0800

    Random complex table

---
+++ sql_scripts/complex.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `complex` (
+  `id` int(5) unsigned NOT NULL,
+  `f1` char,
+  `f2` float(6,3),
+  `f3` ENUM('small', 'medium', 'large'),
+  `f4` TIME,
+  `f5` TIMESTAMP,
+  `f6` DATETIME,
+  `f7` DATE,
+  `f8` DECIMAL(5,2),
+  `f9` JSON,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+
+INSERT INTO `complex` (`id`, `f1`, `f2`, `f3`, `f4`, `f5`, `f6`, `f7`, `f8`, `f9`) VALUES (1, 'A', 100.109, 'small', '01:30:59', '2020-01-01 10:10:10-08:00', '2018-11-23 23:49:23' ,'2020-12-10', -250.99, '{"k1": "value", "k2": 10}');
